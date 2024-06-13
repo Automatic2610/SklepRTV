afa3ee7cc6d19320699f323d75e87308ec989556
AdamTheProgrammer <48458422+awawrzyk@users.noreply.github.com>
2024-06-13 19:33:41 +0200

    Moduł zamówień

---
+++ SklepRTV.MVC/Controllers/OrderController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SklepRTV.Model;
+using SklepRTV.MVC.Data;
+using System.Text.Json;
+
+namespace SklepRTV.MVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Checkout()
+        {
+            var cartJson = HttpContext.Session.GetString("Cart");
+            Cart cart = cartJson != null ? JsonSerializer.Deserialize<Cart>(cartJson) : new Cart();
+
+            if (!cart.Items.Any())
+            {
+                return RedirectToAction("Index", "Cart");
+            }
+
+
+            return View(cart);
+        }
+
+        public ActionResult PlaceOrder(string customerName, string customerEmail, AddressDetails customerAddress )
+        {
+            var cartJson = HttpContext.Session.GetString("Cart");
+            Cart cart = cartJson != null ? JsonSerializer.Deserialize<Cart>(cartJson) : new Cart();
+
+            if (cart.Items.Any())
+            {
+                var order = new Order
+                {
+                    Id = Guid.NewGuid(),
+                    OrderDate = DateTime.Now,
+                    CustomerName = customerName,
+                    CustomerEmail = customerEmail,
+                    CustomerAddress = customerAddress,
+                    TotalAmount = cart.CalculateTotal()
+                };
+
+                foreach (var item in cart.Items) 
+                {
+                    order.OrderItems.Add(new OrderItem
+                    {
+                        Id = Guid.NewGuid(),
+                        OrderId = order.Id,
+                        ProductId = item.Product.Id,
+                        ProductName = item.Product.name,
+                        Quantity = item.Quantity,
+                        Price = item.Product.price
+                    });
+                }
+
+                _db.Orders.Add(order);
+                _db.SaveChanges();
+
+                HttpContext.Session.Remove("Cart");
+
+                return RedirectToAction("OrderConfirmation", new { order = order.Id });
+            }
+
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult OrderConfirmation(Guid orderId)
+        {
+            var order = _db.Orders.FirstOrDefault(o => o.Id == orderId);
+
+            if (order == null) 
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(order);
+    }
+}
