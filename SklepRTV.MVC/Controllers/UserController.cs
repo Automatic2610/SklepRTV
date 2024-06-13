25d59c411345ff288316b9a70d988cb76904b076
AdamTheProgrammer <48458422+awawrzyk@users.noreply.github.com>
2024-06-13 22:13:08 +0200

    Zmiany w zarządzaniu użytkownikami

--- SklepRTV.MVC/Controllers/UserController.cs
+++ SklepRTV.MVC/Controllers/UserController.cs
@@ -28,5 +28,62 @@ namespace SklepRTV.MVC.Controllers
             if (user == null) return NotFound();
             return View(user);
         }
+
+        public async Task<IActionResult> AddMemberRole(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            
+            if (user == null) return NotFound();
+            _userManager.AddToRoleAsync(user, "Member");
+            _userManager.UpdateAsync(user);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> AddManagerRole(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            if (user == null) return NotFound();
+            _userManager.AddToRoleAsync(user, "Manager");
+
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> AddAdminRole(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            if (user == null) return NotFound();
+            _userManager.AddToRoleAsync(user, "Admin");
+
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> DeactiveUser(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            if (user == null) return NotFound();
+            user.EmailConfirmed = false;
+
+            _userManager.UpdateAsync(user);
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> ActivateUser(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            if (user == null) return NotFound();
+            user.EmailConfirmed = true;
+
+            _userManager.UpdateAsync(user);
+
+            return View(user);
+        }
     }
 }
