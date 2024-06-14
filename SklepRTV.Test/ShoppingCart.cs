5467602e9da24cd6a54e7b582b39a6aeb0ab455a
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-06-14 21:53:07 +0200

    Dodanie testów jednostkowych dla Koszyka

--- SklepRTV.Test/ShoppingCart.cs
+++ SklepRTV.Test/ShoppingCart.cs
@@ -19,5 +19,31 @@ namespace SklepRTV.Test
             Assert.Equal(1, items.Count);
 
         }
-    }
+		[Fact]
+		public void RemoveOneItemFromCart()
+		{
+			var cart = new Cart();
+			cart.AddItem(new Product(), 8);
+			var items = cart.Items.ToList();
+			foreach (var item in items)
+			{
+				cart.RemoveItem(item.Product.Id);
+			}
+			Assert.Equal(0, cart.Items.Count);
+		}
+		[Fact]
+		public void CalculateItemsFromCart()
+		{
+			var cart = new Cart();
+			cart.AddItem(new Product(), 1);
+			var items = cart.Items;
+			decimal totalPrice = 0;
+			foreach(var item in items)
+			{
+				totalPrice += item.Product.price;
+			}
+			decimal total = cart.CalculateTotal();
+			Assert.Equal(total, totalPrice);
+		}
+	}
 }
