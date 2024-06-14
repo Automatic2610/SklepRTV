d7d632b006a21c3bc35a439af1913a8d93b206c7
AdamTheProgrammer <48458422+awawrzyk@users.noreply.github.com>
2024-06-14 14:35:28 +0200

    Dodanie projektu testów jednostokowych/ Pierwszy test

---
+++ SklepRTV.Test/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using SklepRTV.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepRTV.Test
+{
+    public class ShoppingCart
+    {
+        [Fact]
+        public void AddOneItemToCart()
+        {
+            var cart = new Cart();
+            cart.AddItem(new Product(), 1);
+            var items = cart.Items;
+
+            Assert.Equal(1, items.Count);
+
+        }
+    }
+}
