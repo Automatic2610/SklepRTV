9a9fc95344c4dc4c75bd31fa127319a81c3e3424
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-06-17 13:36:08 +0200

    Dodanie testów jednostkowych dla Country

---
+++ SklepRTV.Test/CountryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SklepRTV.Model;
+namespace SklepRTV.Test
+{
+	public class CountryTest
+	{
+		[Fact]
+		public void CountryTest_SetIdCountry()
+		{
+			var country = new Country();
+			var idC = 5;
+			country.idCountry = idC;
+			Assert.Equal(idC, country.idCountry);
+		}
+		[Fact]
+		public void CountryTest_SetCountry()
+		{
+			var country = new Country();
+			var nameC = "Polska";
+			country.country = nameC;
+			Assert.Equal(nameC, country.country);
+		}
+	}
+}
