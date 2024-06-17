cb4225620f0a0a0c3c56453c409e063477dd9197
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-06-17 13:36:38 +0200

    Dodanie testów jednostkowych dla OrderItem

---
+++ SklepRTV.Test/OrderItemTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SklepRTV.Model;
+
+namespace SklepRTV.Test
+{
+
+
+	public class OrderItemTest
+	{
+		[Fact]
+		public void OrderItemTest_SetProductName()
+		{
+			var orderItem = new OrderItem();
+			var productName = "Suszarka Philips";
+			orderItem.ProductName = productName;
+			Assert.Equal(productName, orderItem.ProductName);
+		}
+		[Fact]
+		public void OrderItemTest_SetQuantity()
+		{
+			var orderItem = new OrderItem();
+			var quantity = 90;
+			orderItem.Quantity = quantity;
+			Assert.Equal(quantity, orderItem.Quantity);
+		}
+		[Fact]
+		public void OrderItemTest_SetPrice()
+		{
+			var orderItem = new OrderItem();
+			decimal price = 900;
+			orderItem.Price = price;
+			Assert.Equal(price, orderItem.Price);
+		}
+	}
+}
