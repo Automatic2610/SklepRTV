ce30b9db476c69a50197fd03929189e3bec42db2
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-06-17 13:35:06 +0200

    Dodanie testów jednostkowych dla ContactDetails

---
+++ SklepRTV.Test/ContactDetailsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SklepRTV.Model;
+
+namespace SklepRTV.Test
+{
+	public class ContactDetailsTest
+	{
+		[Fact]
+		public void ContactDetails_SetEmail()
+		{
+			var contactDetails = new ContactDetails();
+			var email = "mateusz@example.ex";
+			contactDetails.email = email;
+			Assert.Equal(email,contactDetails.email);
+		}
+		[Fact]
+		public void ContactDetails_SetPhone()
+		{
+			var contactDetails = new ContactDetails();
+			var phone = "123456789";
+			contactDetails.phone = phone;
+			Assert.Equal(phone,contactDetails.phone);
+		}
+
+	}
+}
