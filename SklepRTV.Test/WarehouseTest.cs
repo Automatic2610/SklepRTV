fd5698b2373d866a77f10cb4e2edd011efb75582
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-06-17 13:38:05 +0200

    Dodanie testów jednostkowych dla Warehouse

---
+++ SklepRTV.Test/WarehouseTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SklepRTV.Model;
+
+namespace SklepRTV.Test
+{
+	public class WarehouseTest
+	{
+		[Fact]
+		public void Warehouse_SetName()
+		{
+			var warehouse = new Warehouse();
+			var name = "Kacper";
+			warehouse.name = name;
+			Assert.Equal(name, warehouse.name);
+
+		}
+	}
+}
