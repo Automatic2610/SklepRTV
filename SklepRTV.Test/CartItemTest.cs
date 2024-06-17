0341abfa4b360fc38f6073eb513b83a9a7b68b9c
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-06-17 13:33:43 +0200

    Dodanie testów jednostkowych do CartItem

---
+++ SklepRTV.Test/CartItemTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SklepRTV.Model;
+
+namespace SklepRTV.Test
+{
+	public class CartItemTest
+	{
+		[Fact]
+		public  void CartItem_SetQuantity() 
+		{
+			var cartItem = new CartItem();
+			var quantity = 5;
+			cartItem.Quantity = quantity;
+			Assert.Equal(quantity, cartItem.Quantity);
+		}
+	}
+}
