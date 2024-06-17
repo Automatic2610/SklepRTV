9833c8cf617632e6e6752d3ec7b79f9503459d1c
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-06-17 13:37:44 +0200

    Dodanie testów jednostkowych dla UserType

---
+++ SklepRTV.Test/UsertypeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SklepRTV.Model;
+
+namespace SklepRTV.Test
+{
+	public class UsertypeTest
+	{
+		[Fact]
+		public void UserType_setType()
+		{
+			var userType = new UserType();
+			var id = 1;
+			userType.id = id;
+			Assert.Equal(id, userType.id);
+		}
+		[Fact]
+		public void UserType_SetName()
+		{
+			var userType = new UserType();
+			var name = "Gabrysia";
+			userType.Name = name;
+			Assert.Equal(name, userType.Name);
+		}
+	}
+}
