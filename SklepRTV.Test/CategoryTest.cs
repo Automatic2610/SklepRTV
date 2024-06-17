350493c333072e1638578c4e2c35ef4ad1886121
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-06-17 13:34:11 +0200

    Dodanie testów jednostkowych  dla Category

---
+++ SklepRTV.Test/CategoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SklepRTV.Model;
+namespace SklepRTV.Test
+{
+	public class CategoryTest
+	{
+		/* ustawianie jest ustawione na prywatne
+		[Fact]
+		public void CategoryTest_SetId()
+		{
+			var categoryTest= new Category();
+			var id = 5;
+			categoryTest.Id = id;
+			Assert.Equal(id, categoryTest.Id);
+		}
+		*/
+		[Fact]
+		public void CategoryTest_SetName()
+		{
+			var categoryTest = new Category();
+			var name = "Mateusz";
+			categoryTest.Name = name;
+			Assert.Equal(name, categoryTest.Name);
+		}
+	}
+}
