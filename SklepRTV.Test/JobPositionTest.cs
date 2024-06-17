80b1239f157bb3b42663ce9123f17048bf6a9331
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-06-17 13:36:22 +0200

    Dodanie testów jednostkowych dla JobPosition

---
+++ SklepRTV.Test/JobPositionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SklepRTV.Model;
+
+namespace SklepRTV.Test
+{
+	public class JobPositionTest
+	{
+		[Fact]
+		public void JobPositionTest_SetId()
+		{
+			var jobPosition = new JobPosition();
+			var id = 5;
+			jobPosition.id = id;
+			Assert.Equal(id, jobPosition.id);
+		}
+		[Fact]
+		public void JobPosiotionTest_SetName()
+		{
+			var jobPosition = new JobPosition();
+			var name = "Szymon";
+			jobPosition.name = name;
+			Assert.Equal(name, jobPosition.name);
+		}
+	}
+}
