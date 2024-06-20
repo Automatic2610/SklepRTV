cebdf9d8fb1979c3a5ef7ae5241ac024e517269f
Mateusz W <121889060+Automatic2610@users.noreply.github.com>
2024-06-20 22:47:54 +0200

    Interfejs potrzebny do Testu CategoryControllerTest

---
+++ SklepRTV.Database/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SklepRTV.Model
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Category> Categories { get; set; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken =
+       default);
+    }
+}
